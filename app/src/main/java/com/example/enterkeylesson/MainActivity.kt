538d0671363584746cd7d4b32ca79596e0ea146e
yucheng0 <yucheng0939@gmail.com>
2020-09-12 14:02:23 +0800

    init ok 加註多

--- app/src/main/java/com/example/enterkeylesson/MainActivity.kt
+++ app/src/main/java/com/example/enterkeylesson/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.enterkeylesson
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView.OnEditorActionListener
@@ -11,7 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 原稿-正統寫法
+// 編輯器最簡單的寫法
+        editText.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                //dosomething  做事情區
+                println("最簡單")
+                true
+            }
+            false
+           }
+
+
+ /*       editText.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                println ("HiHI 最簡單")
+                true
+            } else {
+                false
+            }
+        }  */
+
+
+
+
+ /*       // 原稿-正統寫法
         editText.setOnEditorActionListener(OnEditorActionListener { textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE) {
                 //dosomething
@@ -19,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 return@OnEditorActionListener true
             }
               false
-    })
+    })  */
 
 
 /* 另類寫法
